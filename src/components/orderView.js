165f589aaa436ff6a4a1daae066a45fd036c89d9
VeroFlores <veritocalla@gmail.com>
2021-01-09 10:50:52 -0500

    move inputs form to set order

--- src/components/orderView.js
+++ src/components/orderView.js
@@ -5,19 +5,8 @@ import { db } from '../firebase';
 import '../styles/orderView.css'
 
 export const OrderView = () => {
-  const options = ["Select a table", "A1", "A2", "A3"];
   const [typeFood, setTypeFood] = useState('desayuno');
-  const initialStateValues = {
-    client: '',
-    table: '',
-  };
-  const [values, setValues] = useState(initialStateValues);
-
-  const handleInputChange = (e) => {
-    const {name, value} = e.target;
-    setValues({...values, [name]: value});
-  }
-  const addOrder = (order) => {
+  const addOrder = (order,values) => {
     const itemsOrder = order.map((element) => {
       return element['description'];
     });
@@ -33,32 +22,13 @@ export const OrderView = () => {
   };
   return(
     <section className="order-view-section">
-      <div className="input-section">
-          <input className="input"
-          type="text"
-          placeholder="Client name"
-          name="client"
-          onChange={handleInputChange}
-          // value={client}
-          />
-          <select className="select" name="table"
-            onChange={handleInputChange}>  
-
-            {options.map(option => {
-              return <option value={option} key={option} >{option}</option>})}
-          </select>
-      </div>
-
-      <div className='info-section'>
-      <InfoClient infoClient={values}/>
-      </div>
 
       <div className='btn-section'>
           <button className="button menu" onClick={()=>{setTypeFood('desayuno')}}>Desayuno</button>
           <button className="button menu" onClick={()=>{setTypeFood('almuerzo y cena')}}>Almuerzo y cena</button>
       </div>
 
-      <SetOrder typeFood={typeFood} addOrder={addOrder} infoClient={values}/>
+      <SetOrder typeFood={typeFood} addOrder={addOrder}/>
     </section>
   )
 }
--- src/components/setOrder.js
+++ src/components/setOrder.js
@@ -9,6 +9,18 @@ export const SetOrder = (props) => {
   
   const [menu, setMenu] = useState([]);
   const [order, setOrder] = useState([]);
+  const options = ["Select a table", "A1", "A2", "A3"];
+  
+  const initialStateValues = {
+    client: '',
+    table: '',
+  };
+  const [values, setValues] = useState(initialStateValues);
+
+  const handleInputChange = (e) => {
+    const {name, value} = e.target;
+    setValues({...values, [name]: value});
+  }
 
   const selectProduct = (item) => {
     setOrder([...order, item]);
@@ -32,6 +44,7 @@ export const SetOrder = (props) => {
 
   const cleanOrder = () => {
     setOrder([]);
+    setValues(initialStateValues);
   };
 
   return (
@@ -41,13 +54,31 @@ export const SetOrder = (props) => {
       </ul>
       <ul className="display-list-order">
         <div>
+        <div className="input-section">
+          <input className="input"
+          type="text"
+          placeholder="Client name"
+          name="client"
+          onChange={handleInputChange}
+          value={values.client}
+          />
+          <select className="select" 
+            name="table"
+            onChange={handleInputChange}
+            value={values.table}>  
+
+            {options.map(option => {
+              return <option value={option} key={option} >{option}</option>})}
+          </select>
+      </div>
+
           {order.map((item, index) => <OrderList key = {'o'+ index} itemProduct = {item} onDelete={()=>onDeleteOrderList(index)}/>)}
         </div>
         <div className="price-total">Total S/. {totalOrder}</div>
         <button 
           className ='button' 
           onClick={() => {
-            props.addOrder(order);
+            props.addOrder(order,values);
             cleanOrder();
           }}
         >Tomar pedido</button>
